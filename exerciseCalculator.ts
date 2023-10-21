c2e4df620c3b6169f54b7b01468839cfeef6d9e8
imbekrishna <imbekrishna@gmail.com>
2023-10-21 18:03:28 +0530

    inital commit

---
+++ exerciseCalculator.ts
@@ -0,0 +1,46 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercise = (hours: number[], target: number): Result => {
+  const periodLength: number = hours.length;
+  const trainingDays: number = hours.filter((h) => h > 0).length;
+  const average: number = hours.reduce((a, b) => a + b, 0) / periodLength;
+  const success: boolean = average >= target;
+  let rating: number;
+  let ratingDescription: string;
+
+  const targetPer: number = average / target;
+
+  if (targetPer >= 1) {
+    rating = 4;
+    ratingDescription = 'well done';
+  } else if (targetPer > 0.8) {
+    rating = 3;
+    ratingDescription = 'getting there';
+  } else if (targetPer > 0.5) {
+    rating = 2;
+    ratingDescription = 'not too bad';
+  } else {
+    rating = 1;
+    ratingDescription = 'are you even trying?';
+  }
+
+  return {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+};
+
+console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2));
